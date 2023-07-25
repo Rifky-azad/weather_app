181e5051fab5ba8d3ab7d4a2d0d1e0049914dde4
Rifky-azad <asad.18@itfac.mrt.ac.lk>
2023-07-25 23:46:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+  const API_KEY = 'a5f6acb9623b2b9867b7cf726afa4686'; 
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getWeatherData();
+  };
+
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      setWeather(response.data);
+      setError('');
+    } catch (error) {
+      setWeather(null);
+      setError('City not found. Please try again.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={city}
+          onChange={handleInputChange}
+          placeholder="Enter city name"
+          required
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+
+      {weather && (
+        <div className="weather-info">
+          <h2>{weather.name}</h2>
+          <p>Temperature: {Math.round(weather.main.temp - 273.15)}°C</p>
+          <p>Description: {weather.weather[0].description}</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+            alt="Weather Icon"
+          />
+        </div>
+      )}
+
+      {error && <p className="error-message">{error}</p>}
     </div>
   );
 }
